f6ae4ff0855c32d4c615b228c965c40a7a8b472b
Felipe Spinolo <fspinol0@gmail.com>
2015-02-27 21:20:01 -0500

    scanner working

--- Planogrammer/app/src/main/java/com/example/felipe/planogrammer/MainActivity.java
+++ Planogrammer/app/src/main/java/com/example/felipe/planogrammer/MainActivity.java
@@ -1,18 +1,41 @@
 package com.example.felipe.planogrammer;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(this);
     }
+
+    public void onClick(View v) {
+
+        if (v.getId() == R.id.button) {
+            startActivity(new Intent(MainActivity.this, ScannerPage.class));
+        }
+
+    }
+
+
+
+
+
+
+
+
 
 
     @Override

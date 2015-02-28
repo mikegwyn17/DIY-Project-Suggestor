0ec729ee18b63c7970e28a3d8e83ce58cc5abc11
Felipe Spinolo <fspinol0@gmail.com>
2015-02-28 10:54:39 -0500

    Added theme and first screen

--- Planogrammer/app/src/main/java/com/example/felipe/planogrammer/MainActivity.java
+++ Planogrammer/app/src/main/java/com/example/felipe/planogrammer/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.felipe.planogrammer;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
@@ -9,7 +10,7 @@ import android.view.View;
 import android.widget.Button;
 
 
-public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+public class MainActivity extends Activity implements View.OnClickListener {
 
     private Button button;
 
@@ -27,27 +28,5 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
             startActivity(new Intent(MainActivity.this, JSONActivity.class));
         }
 
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 }
